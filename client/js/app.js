86df846b82187668ed6d939a1b9157b02dd845f9
Dadi Sigursveinn <dadi.sigursveinn@gmail.com>
2017-01-25 23:30:58 +0000

    Rectangle is now colored

--- client/js/app.js
+++ client/js/app.js
@@ -1,59 +1,56 @@
 $( document ).ready(function() {
-var settings = {
-    canvasObj: document.getElementById("myCanvas"),
-    nextObject: "Rectangle",
-    nextColor: "Black",
-    currentShape: undefined,
-    shapes: []
-};
+    var settings = {
+        canvasObj: document.getElementById("myCanvas"),
+        nextObject: "Rectangle",
+        nextColor: $('input[name=color]:checked').val(),
+        currentShape: undefined,
+        shapes: []
+    };
 
-$("#myCanvas").mousedown(function(e) {
-    var shape = undefined;
-    var context = settings.canvasObj.getContext("2d");
-    //console.log("Mouse down!");
-    // mouse possiton source
-    // http://stackoverflow.com/questions/3234256/find-mouse-position-relative-to-element
-    var x = e.pageX - $('#myCanvas').offset().left;
-    var y = e.pageY - $('#myCanvas').offset().top;
-
-    if (settings.nextObject == "Circle") {
-        shape = new Circle(/* TODO: Fynna x og y */settings.nextColor);
-    } else if (settings.nextObject == "Rectangle") {
-        settings.currentShape == "Rectangle"
-        console.log("Drawing Rectangle!");
-        shape = new Rectangle(x, y, settings.nextColor);
-    }
-    //...
-    settings.currentShape = shape;
-    settings.shapes.push(shape)
-    shape.draw(context);
-});
+    $("#myCanvas").mousedown(function(e) {
+        var shape = undefined;
+        var context = settings.canvasObj.getContext("2d");
+        var x = e.pageX - this.offsetLeft;
+        var y = e.pageY - this.offsetTop;
+        settings.nextColor = $('input[name=color]:checked').val();
+        if (settings.nextObject == "Circle") {
+            shape = new Circle(/* TODO: Fynna x og y */settings.nextColor);
+        } else if (settings.nextObject == "Rectangle") {
+            settings.currentShape == "Rectangle"
+            console.log("Drawing Rectangle!");
+            shape = new Rectangle(x, y, settings.nextColor);
+        }
+        //...
+        settings.currentShape = shape;
+        settings.shapes.push(shape)
+        shape.draw(context);
+    });
 
 
 
-$("#myCanvas").mousemove( function(e) {
-    console.log("mouse moved")
+    $("#myCanvas").mousemove( function(e) {
+        //console.log("mouse moved")
         if(settings.currentShape !== undefined) {
-            var x = e.pageX - $('#myCanvas').offset().left;
-            var y = e.pageY - $('#myCanvas').offset().top;
+            var x = e.pageX - this.offsetLeft;
+            var y = e.pageY - this.offsetTop;
             settings.currentShape.setEnd(x, y)
             drawAll();
         }
-});
+    });
 
-function drawAll() {
-    var context = settings.canvasObj.getContext("2d");
-    // TODO: clear the canvasObj
-    context.clearRect(0, 0, settings.canvasObj.width, settings.canvasObj.height);
-    // TODO: draw all from array
-    //console.log(settings.shapes)
-    for (var i = 0; i < settings.shapes.length; i++) {
-        settings.shapes[i].draw(context);
+    function drawAll() {
+        var context = settings.canvasObj.getContext("2d");
+        // TODO: clear the canvasObj
+        context.clearRect(0, 0, settings.canvasObj.width, settings.canvasObj.height);
+        // TODO: draw all from array
+        //console.log(settings.shapes)
+        for (var i = 0; i < settings.shapes.length; i++) {
+            settings.shapes[i].draw(context);
+        }
+
     }
 
-}
-
-$("#myCanvas").mouseup( function(e) {
-    settings.currentShape = undefined;
-});
+    $("#myCanvas").mouseup( function(e) {
+        settings.currentShape = undefined;
+    });
 });
